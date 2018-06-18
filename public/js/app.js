9d2b8d27bdfa139db7677282baf6f5906721e453
Talitha Voortman <braveblueberry@live.nl>
2018-06-18 13:46:43 +0200

    Put in the other inputs in editTask

--- public/js/app.js
+++ public/js/app.js
@@ -2,25 +2,28 @@ var data = {
     page: "overview",
     nothome: "hoi",
     tasks: [],
-    currentTask: "",
-    nameTask: ""
+    currentTask: ""
 }
 
 var app = new Vue({
     el: "#app",
     data: data,
     methods: {
+        //Go to edit task page with currentTask
         showEditTask: function (task, event) {
             app.currentTask = task;
             app.page = "editTask";
         },
+        //Go to the overview
         overview: function(event) {
             app.init();
             app.page = "overview";
         },
+        //Go to new task page
         showNewTask: function(event) {
             app.page = "newTask";
         },
+        //Get all tasks
         init: function(event) {
             axios.get('/api/tasks')
                 .then(function(response) {
--- resources/views/editTask.blade.php
+++ resources/views/editTask.blade.php
@@ -4,4 +4,7 @@
 Editing <b>@{{ currentTask.title }}</b>
 <br />
 <br />
+<input v-model="currentTask.title"/>
 <input v-model="currentTask.description"/>
+<input v-model="currentTask.starts_at"/>
+<input type="datetime-local" v-model="currentTask.ends_at"/>
